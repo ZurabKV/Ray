ec4f06563db3e5bef614d899eee4012d8937ebc7
Zurab Kvirikashvili <z.kvirikashvili@godeltech.com>
2020-01-13 14:54:11 +0300

    aaa

--- Screen.cs
+++ Screen.cs
@@ -17,11 +17,13 @@ namespace Ray
        
         public Screen()
         {
-            SquareA = new Walls(50, 25, 0, 0, '#', ConsoleColor.DarkCyan);
-            light = new Light(new Pixel(20, 12, 'O', ConsoleColor.Cyan), '.', ConsoleColor.DarkYellow);
+            SquareA = new Walls(60, 30, 0, 0, '#', ConsoleColor.DarkCyan);
+            light = new Light(new Pixel(20, 20, 'O', ConsoleColor.Cyan), '.', ConsoleColor.DarkYellow);
 
             obsticle = new Obsticle('%', ConsoleColor.Green);
-            obsticle.AddLineBody(new Pixel(33, 6, '%', ConsoleColor.DarkGreen), new Pixel(40, 20));
+            obsticle.AddLineBody(new Pixel(33, 6), new Pixel(10, 20));
+            obsticle.AddLineBody(new Pixel(53, 20), new Pixel(20, 20));
+            obsticle.AddLineBody(new Pixel(13, 47), new Pixel(30, 20));
 
         }
     }
